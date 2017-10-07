1e804610236cf18dbfd5ee31b02ac1901c789b85
bryoncline <bryoncline@yahoo.com>
2017-10-07 16:28:12 -0700

    creating new API

--- BackendAPI/Program.cs
+++ BackendAPI/Program.cs
@@ -1,21 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
-namespace TodoBackend.Api
+namespace BackendAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.Title = "TodoBackend.Api";
-
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://+:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
